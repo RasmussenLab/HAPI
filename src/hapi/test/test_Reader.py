2d3d8f0a62309a56fde3a3ece358ae0dd6d23d42
las02 <lasse101010@gmail.com>
2024-12-06 13:00:41 +0100

    Update gitignore and improve test_Reader

--- src/hapi/test/test_Reader.py
+++ src/hapi/test/test_Reader.py
@@ -1,4 +1,5 @@
-from hapi.utils.yamlreader import YamlReader
+from hapi.utils.reader_of_yaml import YamlReader
+
 
 def test_yamlreader():
     reader = YamlReader("./testfiles/testconfig.yaml")
@@ -7,8 +8,12 @@ def test_yamlreader():
     top4_snps_list = reader.top4_snps_list
     chrom = reader.chromosome
 
-    assert (position_list_reference == [[46414944, 46414975], [46414945, 46414976],
-                                        [46414946, 46414977], [46414947, 46414978]])
-    assert (position_list_deletion == [46414943])
-    assert (top4_snps_list == ["rs113341849", "rs113010081", "rs11574435", "rs79815064"])
-    assert (chrom == str(3))
+    assert position_list_reference == [
+        [46414944, 46414975],
+        [46414945, 46414976],
+        [46414946, 46414977],
+        [46414947, 46414978],
+    ]
+    assert position_list_deletion == [46414943]
+    assert top4_snps_list == ["rs113341849", "rs113010081", "rs11574435", "rs79815064"]
+    assert chrom == str(3)
